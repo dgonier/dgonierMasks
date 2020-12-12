aed3fd728d39f03aa33508321b61c45f9ddc985e
dgonier <devin.gonier@thedgcgroup.com>
2020-12-12 11:51:18 -0700

    frozen models

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ const demo = async (img) => {
   // Warmup the model. This isn't necessary, but makes the first prediction
   // faster. Call `dispose` to release the WebGL memory allocated for the return
   // value of `predict`.
-  my_model.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
-
+  // my_model.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
+  my_model.execute(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
   status('');
 
   // Make a prediction through the locally hosted cat.jpg.
