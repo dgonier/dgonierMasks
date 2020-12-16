686e2946d528b2f061f97ef8677134b9b58f6817
dgonier <devin.gonier@thedgcgroup.com>
2020-12-15 20:30:20 -0700

    hmmm what happened

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ const IMAGE_SIZE = 192;
 const loadModel = async () => {
   const model = await tf.loadLayersModel('./model.json');
   const webcamElement = document.getElementById('video');
+  const webcam = await tf.data.webcam(webcamElement);
+// model.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
+
   let noMask = true
   const sleep = m => new Promise(r => setTimeout(r, m))
 
@@ -39,21 +42,46 @@ const loadModel = async () => {
 
     // Reshape to a single-element batch so we can pass it to predict.
     const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
-    // console.log(batched.dataSync())
+    console.log(batched.dataSync())
     const result = await model.predict(batched);
     const probs = result.dataSync()
 
     if (probs[0] > 0.95) {
       noMask = false
     }
+    // console.log(result)
+    // console.log(result.dataSync())
 
     document.getElementById('console').innerText = `
       ${probs[0]}% of wearing mask
     `
 
+    // document.getElementById('console').innerText = `
+    //   prediction: ${result[0].className}\n
+    //   probability: ${result[0].probability}
+    // `;
+    // status(`${result[0].className}`)
+    // Dispose the tensor to release the memory.
+    // img.dispose();
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
     await sleep(1000);
     // await tf.nextFrame();
   }
 }
 
 loadModel()
+
+
+
+
+// todo note change to setInterval
+// window.setTimeout(function(){
+//   console.log('hi')
+//   var context = canvas.getContext("2d").drawImage(video, 0, 0, 640, 480);
+//   let input = canvas.toDataURL("image/jpg")
+//   let img = document.getElementById('cat');
+//   img.src = input;
+//   demo(img)
+// }, 3000)
